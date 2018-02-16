e2f9a8314bfadfb7534d19054044d48f566380ca
Tim Hess <thess@pivotal.io>
2018-02-16 13:31:31 -0600

    add sentiment analysis endpoint

--- src/CredBulb/Controllers/HomeController.cs
+++ src/CredBulb/Controllers/HomeController.cs
@@ -5,7 +5,9 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Steeltoe.Common.Http;
 using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,16 +15,19 @@ namespace CredBulb.Controllers
 {
     public class HomeController : Controller
     {
-        private NewColorCommand colorCommand;
+        private NewColorCommand _colorCommand;
         private static HttpClient _httpClient;
         private JsonSerializerSettings _jsonSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
         private string _iftttUrl = "https://maker.ifttt.com/trigger/custom_light_up/with/key/{0}";
+        private string _sentimentUrl = "https://eastus2.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
+        private string _cognitiveKey;
 
         public HomeController(IConfiguration config, NewColorCommand newColorCommand)
         {
-            colorCommand = newColorCommand;
+            _colorCommand = newColorCommand;
             _httpClient = new HttpClient();
             _iftttUrl = string.Format(_iftttUrl, config.GetValue(typeof(string), "iftttKey"));
+            _cognitiveKey = config.GetValue<string>("cognitiveServicesKey");
         }
 
         public IActionResult Index()
@@ -34,7 +39,7 @@ namespace CredBulb.Controllers
         public async Task<IActionResult> CredHubColorize()
         {
             // call credhub to generate a hex password
-            var color = await colorCommand.ExecuteAsync();
+            var color = await _colorCommand.ExecuteAsync();
 
             // call ifttt to colorize
             var response = await _httpClient.PostAsJsonAsync($"{_iftttUrl}", 
@@ -44,8 +49,56 @@ namespace CredBulb.Controllers
                 _jsonSettings);
 
             // return results
-            Thread.Sleep(1000);
+            Thread.Sleep(1500);
             return Json(color);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LightByText([FromBody]string text)
+        {
+            // post text to cognitive services api
+            var message = new HttpRequestMessage {
+                RequestUri = new System.Uri(_sentimentUrl),
+                Method = HttpMethod.Post,
+                Headers =
+                {
+                    { "Ocp-Apim-Subscription-Key", _cognitiveKey },
+                    { "Accept", "application/json" }
+                },
+                Content = new StringContent($"{{\"documents\":[{{\"language\":\"en\",\"id\":\"1\",\"text\":\"{text}\"}}]}}", Encoding.UTF8, "application/json")
+            };
+            var sentimentHttpResponse = await _httpClient.SendAsync(message);
+            SentimentResponse sentimentResponseData = await sentimentHttpResponse.Content.ReadAsJsonAsync<SentimentResponse>();
+            sentimentResponseData.Documents.First().TryGetValue("score", out string scoreResponse);
+            var sentiment = double.Parse(scoreResponse);
+
+            // turn result into color
+            string color;
+            if (sentiment < .3)
+            {
+                color = "FF0000";
+            }
+            else if(sentiment >= .3 && sentiment < .7)
+            {
+                color = "0000FF";
+            }
+            else
+            {
+                color = "00FF00";
+            }
+
+            // post to ifttt
+            var response = await _httpClient.PostAsJsonAsync($"{_iftttUrl}",
+                new IftttWebhookPayload
+                {
+                    Value1 = color,
+                    Value2 = "1"
+                },
+                _jsonSettings);
+
+            // return sentiment + color value
+            Thread.Sleep(1500);
+            return Json($"'{text}' has a sentiment value of {sentiment}, which evaluates to {color}");
         }
 
         public IActionResult Error()
---
+++ src/CredBulb/Models/SentimentResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CredBulb.Models
+{
+    public class SentimentResponse
+    {
+        public List<Dictionary<string, string>> Documents { get; set; }
+    }
+}
