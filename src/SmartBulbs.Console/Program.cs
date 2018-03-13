07a980e8213f8674b6e853921dd45071fec2ab1c
Tim Hess <thess@pivotal.io>
2018-03-13 12:04:19 -0500

    Discovery isn't working yet

--- src/SmartBulbs.Console/Program.cs
+++ src/SmartBulbs.Console/Program.cs
@@ -1,8 +1,15 @@
 ﻿using LinqToTwitter;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using Pivotal.Discovery.Client;
+using Pivotal.Discovery.Eureka;
 using SmartBulbs.Common;
+using Steeltoe.CloudFoundry.Connector;
+using Steeltoe.CloudFoundry.Connector.Services;
+using Steeltoe.Common.Discovery;
 using Steeltoe.Common.Http;
 using Steeltoe.Discovery.Eureka;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
@@ -22,6 +29,30 @@ namespace TwitterMonitor
 
         public static async Task Main(string[] args)
         {
+            Environment.SetEnvironmentVariable("VCAP_SERVICES", @"{
+      'p-service-registry': [
+        {
+          'credentials': {
+            'uri': 'https://eureka-aefc0bdc-3405-4a90-b977-94738da5a8c4.apps.beet.springapps.io',
+            'client_secret': 'tBlAZFxVXTA2',
+            'client_id': 'p-service-registry-5e3446d6-912e-4d24-b6b8-e4825c5d8d95',
+            'access_token_uri': 'https://p-spring-cloud-services.uaa.cf.beet.springapps.io/oauth/token'
+          },
+          'syslog_drain_url': null,
+          'volume_mounts': [],
+          'label': 'p-service-registry',
+          'provider': null,
+          'plan': 'standard',
+          'name': 'myDiscoveryService',
+          'tags': [
+            'eureka',
+            'discovery',
+            'registry',
+            'spring-cloud'
+          ]
+        }
+      ]
+    }");
             // Bootstrap the app
             var builder = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
@@ -33,22 +64,38 @@ namespace TwitterMonitor
             var factory = new LoggerFactory();
             factory.AddConsole(Configuration.GetSection("Logging"));
 
+            var serviceCollection = new ServiceCollection();
+            //IServiceInfo info = Configuration.GetServiceInfo<EurekaServiceInfo>("myDiscoveryService");
+            //EurekaServiceInfo einfo = info as EurekaServiceInfo;
+            //var clientSection = Configuration.GetSection(EurekaClientOptions.EUREKA_CLIENT_CONFIGURATION_PREFIX);
+            //serviceCollection.Configure<EurekaClientOptions>(clientSection);
+            //serviceCollection.PostConfigure<EurekaClientOptions>((options) =>
+            //{
+            //    PivotalEurekaConfigurer.UpdateConfiguration(Configuration, einfo, options);
+            //});
+
+            var services = serviceCollection
+                .AddDiscoveryClient(Configuration)
+                .BuildServiceProvider();
+
+            //var clientConfig = new EurekaClientOptions();
+            //ConfigurationBinder.Bind(Configuration.GetSection("eureka:client"), clientConfig);
+            //EurekaServiceInfo einfo = Configuration.GetServiceInfo<EurekaServiceInfo>("myDiscoveryService");
+            //PivotalEurekaConfigurer.UpdateConfiguration(Configuration, einfo, clientConfig);
+
+            //// Create the Eureka client, start fetching registry in background thread
+            //var client = new DiscoveryClient(clientConfig, null, factory);
+            //var apps = client.Applications;
+            //var api = apps.GetRegisteredApplication("SmartBulbs-Web");
+
+            //if (api?.Instances.Any() != true)
+            //{
+            //    Console.WriteLine($"Eureka Urls attempted: {clientConfig.EurekaServerServiceUrls}");
+            //    throw new Exception("Discovery failed");
+            //}
+
             TwitterCredentials twitterCreds = new TwitterCredentials();
             Configuration.GetSection("Twitter").Bind(twitterCreds);
-
-            // Build Eureka clients config from configuration
-            var discoveryConfig = new EurekaClientConfig();
-            Configuration.GetSection("eureka:client").Bind(discoveryConfig);
-
-            // Create the Eureka client, start fetching registry in background thread
-            var discovery = new DiscoveryClient(discoveryConfig, null, factory);
-            var apps = discovery.Applications;
-            var api = apps.GetRegisteredApplication("SmartBulbs-Web");
-
-            if (api.Count == 0)
-            {
-                throw new Exception("Discovery failed");
-            }
 
             var auth = new SingleUserAuthorizer
             {
@@ -61,30 +108,26 @@ namespace TwitterMonitor
                 }
             };
             await auth.AuthorizeAsync();
-
             var ctx = new TwitterContext(auth);
-
-            var httpClient = new HttpClient();
             ulong sinceId = 1;
+
+            // next line Fails, not able to resolve IOptionsMonitor<EurekaClientOptions>
+            var discoveryClient = services.GetRequiredService<IDiscoveryClient>();
+            //DiscoveryHttpClientHandler _handler = new DiscoveryHttpClientHandler(discoveryClient, factory.CreateLogger<DiscoveryHttpClientHandler>());
+            //var httpClient = new HttpClient(_handler, false);
+            var httpClient = new HttpClient();
 
             // begin monitoring
             Console.WriteLine("Entering the loop...");
             while (true)
             {
+
                 Console.WriteLine("Checking for tweets...");
                 //string searchTerm = "#cfsummit -coldfusion";
                 string searchTerm = "#internationalwomensday2018";
 
                 List<Status> searchResponse =
-                    await
-                    (from search in ctx.Search
-                     where search.Type == SearchType.Search &&
-                           search.Query == searchTerm &&
-                           search.IncludeEntities == true &&
-                           search.TweetMode == TweetMode.Extended && 
-                           search.SinceID == sinceId
-                     select search.Statuses)
-                    .SingleOrDefaultAsync();
+                    await (from s in ctx.Search where s.Query == searchTerm && s.Type == SearchType.Search && s.IncludeEntities == true && s.TweetMode == TweetMode.Extended && s.SinceID == sinceId && s.Count == 5 select s.Statuses).SingleOrDefaultAsync();
 
                 if (searchResponse.Any())
                 {
@@ -97,25 +140,16 @@ namespace TwitterMonitor
                     }
 
                     // post to web api
-                    if (api != null)
+                    var apiResponse = await httpClient.PostAsync("http://SmartBulbs-Web/home/bulktext", new StringContent(JsonConvert.SerializeObject(texts), Encoding.UTF8, "application/json"));
+                    if (apiResponse.IsSuccessStatusCode)
                     {
-                        var apiResponse = await httpClient.PostAsync(api.Instances[0].HomePageUrl + "home/bulktext", new StringContent(JsonConvert.SerializeObject(texts), Encoding.UTF8, "application/json"));
-                        if (apiResponse.IsSuccessStatusCode)
-                        {
-                            var responseBody = await apiResponse.Content.ReadAsJsonAsync<ColorChangeResponse>();
-                            Console.WriteLine($"Aggregate sentiment value was {responseBody.Sentiment} which translates to #{responseBody.HexColor}");
-                        }
-                        else
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine($"Request failed, status code: {apiResponse.StatusCode}");
-                            Console.ForegroundColor = ConsoleColor.White;
-                        }
+                        var responseBody = await apiResponse.Content.ReadAsJsonAsync<ColorChangeResponse>();
+                        Console.WriteLine($"Aggregate sentiment value was {responseBody.Sentiment} which translates to #{responseBody.HexColor}");
                     }
                     else
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"Unable to find api server!");
+                        Console.WriteLine($"Request failed, status code: {apiResponse.StatusCode}");
                         Console.ForegroundColor = ConsoleColor.White;
                     }
                 }
@@ -123,7 +157,7 @@ namespace TwitterMonitor
                 {
                     Console.WriteLine("No new tweets found");
                 }
-                Thread.Sleep(60000);
+                Thread.Sleep(30000);
             }
         }
     }
