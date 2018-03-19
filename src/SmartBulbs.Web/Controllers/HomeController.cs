5a2e1e784c781b87fb3dd86b118547afc1df484f
Tim Hess <thess@pivotal.io>
2018-03-19 15:32:19 -0500

    skip ifttt and call LIFX directly, move Twitter endpoint to a different controller and move some logic out of the controller

--- src/SmartBulbs.Web/Controllers/HomeController.cs
+++ src/SmartBulbs.Web/Controllers/HomeController.cs
@@ -1,21 +1,18 @@
-﻿using LinqToTwitter;
+﻿using LifxIoT.Api;
+using LifxIoT.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using SmartBulbs.Common;
 using SmartBulbs.Web.Hubs;
 using SmartBulbs.Web.Models;
-using Steeltoe.Common.Http;
-using System;
+using SmartBulbs.Web.Services;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace SmartBulbs.Web.Controllers
@@ -25,20 +22,17 @@ namespace SmartBulbs.Web.Controllers
         private NewPasswordCommand _colorCommand;
         private static HttpClient _httpClient;
         private JsonSerializerSettings _jsonSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
-        private string _iftttUrl = "https://maker.ifttt.com/trigger/custom_light_up/with/key/{0}";
-        private string _sentimentUrl = "https://eastus2.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
-        private string _cognitiveKey;
-        private TwitterCredentials _twitterCreds;
         private IHubContext<ObservationHub> _hubContext;
+        private LifxApi _lifxClient;
+        private Utils _utils;
 
-        public HomeController(IConfiguration config, NewPasswordCommand newColorCommand, IOptionsSnapshot<TwitterCredentials> twitterCreds, IHubContext<ObservationHub> hubContext)
+        public HomeController(IConfiguration config, NewPasswordCommand newColorCommand, IHubContext<ObservationHub> hubContext)
         {
             _colorCommand = newColorCommand;
             _httpClient = new HttpClient();
-            _iftttUrl = string.Format(_iftttUrl, config.GetValue(typeof(string), "iftttKey"));
-            _cognitiveKey = config.GetValue<string>("cognitiveServicesKey");
-            _twitterCreds = twitterCreds.Value;
             _hubContext = hubContext;
+            _lifxClient = new LifxApi(config.GetValue<string>("lifxKey"));
+            _utils = new Utils(config.GetValue<string>("cognitiveServicesKey"), _httpClient);
         }
 
         public IActionResult Index()
@@ -56,138 +50,47 @@ namespace SmartBulbs.Web.Controllers
         {
             // call credhub to generate a hex password
             var newPassword = await _colorCommand.ExecuteAsync();
+
+            // hash the password and shift the bits to convert to RGB
             int hash = newPassword.GetHashCode();
             string r = ((hash & 0xFF0000) >> 16).ToString("X2");
             string g = ((hash & 0x00FF00) >> 8).ToString("X2");
             string b = (hash & 0x0000FF).ToString("X2");
-            var color = r + g + b;
 
-            // call ifttt to colorize
-            var ifResponse = await _httpClient.PostAsJsonAsync($"{_iftttUrl}", 
-                new IftttWebhookPayload {
-                    Value1 = color,
-                    Value2 = "1" }, 
-                _jsonSettings);
-            var colorResponse = new ColorChangeResponse { HexColor = color, TextInput = newPassword };
-            await _hubContext.Clients.All.SendAsync("Messages", new List<ColorChangeResponse> { colorResponse });
+            var response = new ColorChangeResponse { HexColor = r + g + b, TextInput = newPassword };
 
-            // return results
-            Thread.Sleep(1500);
-            return Json(colorResponse);
+            await SetColorNotifyObservers(response);
+            return Json(response);
         }
 
         [HttpPost]
         public async Task<IActionResult> LightByText([FromBody]string text)
         {
-            var response = new ColorChangeResponse { TextInput = text };
+            var analysis = await _utils.GetColorAndSentimentFromText(new List<string> { text });
 
-            var analysis = await GetColorAndSentimentFromText(new List<string> { text });
+            var response = new ColorChangeResponse {
+                TextInput = text,
+                Sentiment = analysis.First().Sentiment,
+                HexColor = analysis.First().HexColor
+            };
 
-            response.Sentiment = analysis.First().Sentiment;
-            response.HexColor = analysis.First().HexColor;
-
-            // post to ifttt
-            var ifTTTresponse = await _httpClient.PostAsJsonAsync($"{_iftttUrl}",
-                new IftttWebhookPayload
-                {
-                    Value1 = response.HexColor,
-                    Value2 = "1"
-                },
-                _jsonSettings);
-
-            await _hubContext.Clients.All.SendAsync("Messages", new List<ColorChangeResponse>{ response });
-
-            // return sentiment + color value
-            Thread.Sleep(1500);
+            await SetColorNotifyObservers(response);
             return Json(response);
         }
 
         [HttpPost]
         public async Task<IActionResult> BulkText([FromBody]List<string> texts)
         {
-            var analysis = await BulkSentiment(texts);
+            var analysis = await _utils.BulkSentiment(texts);
 
-            await _hubContext.Clients.All.SendAsync("Messages", GetColorFromTextAndSentiment(analysis));
+            await _hubContext.Clients.All.SendAsync("Messages", _utils.GetColorFromTextAndSentiment(analysis));
 
             var response = new ColorChangeResponse { TextInput = "Bulk Analysis" };
             response.Sentiment = analysis.Average(i => i.Sentiment);
-            response.HexColor = HexColorFromDecimal(response.Sentiment);
+            response.HexColor = _utils.HexColorFromDecimal(response.Sentiment);
 
-            // post to ifttt
-            var ifTTTresponse = await _httpClient.PostAsJsonAsync($"{_iftttUrl}",
-                new IftttWebhookPayload
-                {
-                    Value1 = response.HexColor,
-                    Value2 = "1"
-                },
-                _jsonSettings);
-
-            // return sentiment + color value
+            await SetColorNotifyObservers(response, false);
             return Json(response);
-        }
-
-        private ulong sinceId = 0;
-
-        [HttpPost]
-        public async Task<IActionResult> CheckTwitter()
-        {
-            try
-            {
-                var auth = new SingleUserAuthorizer
-                {
-                    CredentialStore = new InMemoryCredentialStore
-                    {
-                        ConsumerKey = _twitterCreds.ConsumerKey,
-                        ConsumerSecret = _twitterCreds.ConsumerSecret,
-                        OAuthToken = _twitterCreds.AccessToken,
-                        OAuthTokenSecret = _twitterCreds.AccessTokenSecret
-                    }
-                };
-                await auth.AuthorizeAsync();
-
-                var ctx = new TwitterContext(auth);
-                if (auth == null)
-                {
-
-                }
-                string searchTerm = "#cfsummit -coldfusion";
-
-                Search searchResponse =
-                    await
-                    (from search in ctx.Search
-                     where search.Type == SearchType.Search &&
-                           search.ResultType == ResultType.Mixed &&
-                           search.Query == searchTerm &&
-                           search.IncludeEntities == true &&
-                           search.TweetMode == TweetMode.Extended && 
-                           search.Count == 2 && 
-                           search.SinceID == sinceId
-                     select search)
-                    .SingleOrDefaultAsync();
-                sinceId = searchResponse.Statuses.Max(i => i.ID);
-                var texts = searchResponse.Statuses.Select(t => t.FullText);
-                var analyzed = await GetColorAndSentimentFromText(texts);
-
-                var toReturn = new List<EnhancedTwitterStatus>();
-                foreach (var status in searchResponse.Statuses.Select((value, i) => new { i, value }))
-                {
-                    var s = status.value;
-                    var analysis = analyzed.Find(a => a.TextInput == s.FullText);
-                    toReturn.Add(new EnhancedTwitterStatus {
-                        FullText = s.FullText,
-                        CreatedAt = s.CreatedAt,
-                        User = s.User,
-                        SentimentValue = analysis.Sentiment,
-                        HexColor = analysis.HexColor});
-                    await _hubContext.Clients.All.SendAsync("Messages", new List<ColorChangeResponse> { analysis });
-                }
-
-                return Json(toReturn);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
         }
 
         public IActionResult Error()
@@ -195,122 +98,13 @@ namespace SmartBulbs.Web.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        /// <summary>
-        /// Analyze text and provide sentiment analysis and color values for each string
-        /// </summary>
-        /// <param name="texts">Strings to analyze</param>
-        /// <returns>Sentiment analysis and color values with original strings</returns>
-        private async Task<List<ColorChangeResponse>> GetColorAndSentimentFromText(IEnumerable<string> texts)
+        private async Task SetColorNotifyObservers(ColorChangeResponse response, bool? notify = true)
         {
-            return GetColorFromTextAndSentiment(await BulkSentiment(texts));
-        }
-
-        private List<ColorChangeResponse> GetColorFromTextAndSentiment(List<ScoredText> texts)
-        {
-            var toReturn = new List<ColorChangeResponse>();
-            foreach (var r in texts)
+            await _lifxClient.SetState(new All(), new SentState { Color = $"#{response.HexColor}", Duration = 1 });
+            if (notify == true)
             {
-                toReturn.Add(new ColorChangeResponse(r, HexColorFromDecimal(r.Sentiment)));
+                await _hubContext.Clients.All.SendAsync("Messages", new List<ColorChangeResponse> { response });
             }
-
-            return toReturn;
-        }
-
-        /// <summary>
-        /// Call Azure Cognitive Services' Sentiment Analysis Api
-        /// </summary>
-        /// <param name="texts">List of strings to score</param>
-        /// <returns>List of strings with their sentiment scores</returns>
-        private async Task<List<ScoredText>> BulkSentiment(IEnumerable<string> texts)
-        {
-            var toReturn = new List<ScoredText>();
-
-            var i = 1;
-            var docList = string.Empty;
-
-            foreach (var t in texts)
-            {
-                // scrub user content so the api call won't fail
-                var cleanText = t.Replace("\r", "").Replace("\n", "").Replace('"', '\'');
-
-                // build the request body - Cognitive services requires a unique integer per item
-                docList += $"{{\"language\":\"en\",\"id\":\"{i}\",\"text\":\"{cleanText}\"}},";
-                i++;
-            }
-            docList = docList.Substring(0, docList.Length - 1);
-            var message = new HttpRequestMessage
-            {
-                RequestUri = new Uri(_sentimentUrl),
-                Method = HttpMethod.Post,
-                Headers =
-                {
-                    { "Ocp-Apim-Subscription-Key", _cognitiveKey },
-                    { "Accept", "application/json" }
-                },
-                Content = new StringContent($"{{\"documents\":[{docList}]}}", Encoding.UTF8, "application/json")
-            };
-            var sentimentHttpResponse = await _httpClient.SendAsync(message);
-            SentimentResponse sentimentResponseData = await sentimentHttpResponse.Content.ReadAsJsonAsync<SentimentResponse>();
-            i = 0;
-            foreach (var r in sentimentResponseData.Documents)
-            {
-                var sentimentParsible = r.TryGetValue("score", out string scoreResponse);
-                var sentiment = double.Parse(scoreResponse);
-                toReturn.Add(new ScoredText { TextInput = texts.ElementAt(i), Sentiment = sentiment });
-                i++;
-            }
-
-            return toReturn;
-        }
-
-
-        /// <summary>
-        /// Convert a decimal (between 0 and 1) to an RGB value
-        /// </summary>
-        /// <param name="sentiment">Sentiment value</param>
-        /// <returns>RGB color value</returns>
-        /// <remarks>The scale is red(0) to green(1), with blue values highest at .5 and lower towards 0 or 1</remarks>
-        private string HexColorFromDecimal(double sentiment)
-        {
-            var red = sentiment - 1;
-            var green = sentiment;
-            double blue;
-            if (sentiment < .5)
-            {
-                blue = sentiment;
-            }
-            else if (sentiment == .5)
-            {
-                blue = 1;
-            }
-            else
-            {
-                blue = 1 - sentiment;
-            }
-            return Hexicolor(red) + Hexicolor(green) + Hexicolor(blue);
-        }
-
-        /// <summary>
-        /// Converts a decimal to a hex value
-        /// </summary>
-        /// <param name="value">Value to convert</param>
-        /// <returns>Hex value</returns>
-        /// <remarks>Values are altered to min 0 and max of 255 if outside those bounds</remarks>
-        private string Hexicolor(double value)
-        {
-            if (value < 0)
-            {
-                value *= -1;
-            }
-
-            value *= 255;
-
-            if (value > 255)
-            {
-                value = 255; 
-            }
-
-            return Convert.ToInt16(value).ToString("X2");
         }
     }
 }
---
+++ src/SmartBulbs.Web/Controllers/TwitterController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using LinqToTwitter;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using SmartBulbs.Common;
+using SmartBulbs.Web.Models;
+using SmartBulbs.Web.Services;
+
+namespace SmartBulbs.Web.Controllers
+{
+    public class TwitterController : Controller
+    {
+        private TwitterCredentials _twitterCreds;
+        private Utils _utils;
+
+        public TwitterController(IConfiguration config, IOptionsSnapshot<TwitterCredentials> twitterCreds)
+        {
+            _twitterCreds = twitterCreds.Value;
+            _utils = new Utils(config.GetValue<string>("cognitiveServicesKey"), new HttpClient());
+        }
+
+        private ulong sinceId = 0;
+
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var auth = new SingleUserAuthorizer
+                {
+                    CredentialStore = new InMemoryCredentialStore
+                    {
+                        ConsumerKey = _twitterCreds.ConsumerKey,
+                        ConsumerSecret = _twitterCreds.ConsumerSecret,
+                        OAuthToken = _twitterCreds.AccessToken,
+                        OAuthTokenSecret = _twitterCreds.AccessTokenSecret
+                    }
+                };
+                await auth.AuthorizeAsync();
+
+                var ctx = new TwitterContext(auth);
+                if (auth == null)
+                {
+
+                }
+                string searchTerm = "#cfsummit -coldfusion";
+
+                Search searchResponse =
+                    await
+                    (from search in ctx.Search
+                     where search.Type == SearchType.Search &&
+                           search.ResultType == ResultType.Mixed &&
+                           search.Query == searchTerm &&
+                           search.IncludeEntities == true &&
+                           search.TweetMode == TweetMode.Extended &&
+                           search.Count == 2 &&
+                           search.SinceID == sinceId
+                     select search)
+                    .SingleOrDefaultAsync();
+                sinceId = searchResponse.Statuses.Max(i => i.ID);
+                var texts = searchResponse.Statuses.Select(t => t.FullText);
+                var analyzed = await _utils.GetColorAndSentimentFromText(texts);
+
+                var toReturn = new List<EnhancedTwitterStatus>();
+                foreach (var status in searchResponse.Statuses.Select((value, i) => new { i, value }))
+                {
+                    var s = status.value;
+                    var analysis = analyzed.Find(a => a.TextInput == s.FullText);
+                    toReturn.Add(new EnhancedTwitterStatus
+                    {
+                        FullText = s.FullText,
+                        CreatedAt = s.CreatedAt,
+                        User = s.User,
+                        SentimentValue = analysis.Sentiment,
+                        HexColor = analysis.HexColor
+                    });
+                }
+
+                return Json(toReturn);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
